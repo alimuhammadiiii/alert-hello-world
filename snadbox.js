a9964beee12575fc0cd41098d30d0e7665f06df5
WebPajooh <webpajooh@gmail.com>
2021-05-08 22:02:49 +0430

    Add spices

--- snadbox.js
+++ snadbox.js
@@ -1,5 +1,5 @@
-function myfunction(){
-    alert("Hello World!")
-        
-    
-    }
+function myfunction() {
+    let proverbs = ["When pigs fly", "Another day, another dollar", "What is past, is past", "Haste makes waste", "Nothing hurts like the truth"]
+    let randomProverb = proverbs[Math.floor(Math.random() * proverbs.length)]
+    alert(randomProverb)
+}
